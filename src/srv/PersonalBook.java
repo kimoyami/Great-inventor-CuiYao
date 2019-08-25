fb21fb0436bec9e5a2e0789288ce2edb70d9c2c5
custard-pie <54272970+custard-pie@users.noreply.github.com>
2019-08-25 23:37:55 +0800

    图书馆部分

---
+++ src/srv/PersonalBook.java
@@ -0,0 +1,48 @@
+package srv;
+//私有的借书信息
+ class PersonalBook {
+    private String BOOK_ID;
+    private String BOOK_NAME;
+    private long borrowDate;
+    private long returnDate;
+    private String Borrower;
+
+     PersonalBook(String BOOK_ID, String BOOK_NAME, String Borrower, long borrowDate, long returnDate) {
+        this.BOOK_ID = BOOK_ID;
+        this.BOOK_NAME = BOOK_NAME;
+        this.Borrower = Borrower;
+        this.borrowDate = borrowDate;
+        this.returnDate = returnDate;
+    }
+     public String getBookName() {
+         return BOOK_NAME;
+     }
+     public void setBookName(String BOOK_NAME) {
+         this.BOOK_NAME = BOOK_NAME;
+     }
+     public String getBookID() {
+         return BOOK_ID;
+     }
+     public void setBookID(String BOOK_ID) {
+         this.BOOK_ID = BOOK_ID;
+     }
+     public String getBorrower() {
+         return Borrower;
+     }
+     public void setBorrower(String Borrower) {
+         this.BOOK_ID = Borrower;
+     }
+     public long getborrowDate() {
+         return borrowDate;
+     }
+     public void setborrowDate(long borrowDate) {
+         this.borrowDate = borrowDate;
+     }
+     public long getreturnDate() {
+         return returnDate;
+     }
+     public void setreturnDate(long returnDate) {
+         this.returnDate = returnDate;
+     }
+
+}
