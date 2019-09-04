04deda160e92d9fcf98d12ecd3cc080ae3b86152
AppDoraemon <694458125@qq.com>
2019-09-04 13:57:25 +0800

    Revert "test"
    This reverts commit 45c42801b12d5aee3be743a4b4d5fde19d2e3c1d.

--- src/view/test.java
+++ src/view/test.java
@@ -53,18 +53,8 @@ public class test {
         frame.setSize(1200, 800);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-        browser.loadURL("C:\\Users\\崔峣\\Desktop\\test\\Great-inventor-CuiYao\\login\\index.html");
-        browser.addLoadListener(new LoadAdapter() {
-=======
         browser.loadURL("C:\\Users\\崔峣\\Desktop\\test\\Great-inventor-CuiYao\\html\\index.html");
         browser.addScriptContextListener(new ScriptContextAdapter() {
->>>>>>> Stashed changes
-=======
-        browser.loadURL("F:\\GitHub\\Great-inventor-CuiYao\\html\\index.html");
-        browser.addScriptContextListener(new ScriptContextAdapter() {
->>>>>>> master
             @Override
             public void onScriptContextCreated(ScriptContextEvent event) {
                 Browser browser = event.getBrowser();
